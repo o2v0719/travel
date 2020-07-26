41f12863176fa81a8cfae7299201493dcb59becf
神经蛙 <470408436@qq.com>
2020-07-26 20:21:35 +0800

    change

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App'
 import router from './router'
 // 解决移动端个别浏览器0.3s点击延迟的问题
 import fastClick from 'fastClick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 //此处路径在webpack.base.conf.js中配置过
 import 'styles/reset.css'
@@ -12,6 +14,7 @@ import 'styles/border.css'
 Vue.config.productionTip = false
 
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/Home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img :src="item.imgUrl" class="swiper-img">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        // 循环轮播
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: '//imgs.qunarzz.com/vs_ceph_vcimg/b8c4527c41649814cc4cf86880abba54.jpeg'
+      }, {
+        id: '0002',
+        imgUrl: '//imgs.qunarzz.com/vc/6d/9f/35/b8ad5468f73fd60ec0ced086f6.jpg_92.jpg'
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+// 样式穿透
+.wrapper >>> .swiper-pagination-bullet-active
+  background #fff
+.wrapper
+  // 父级高度随子级内容高度自适应
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 31.25%
+  background #eee
+  .swiper-img
+    width 100%
+</style>
