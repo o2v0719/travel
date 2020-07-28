fb33781a06e199f44a494def9cb26b41bd946af4
神经蛙 <470408436@qq.com>
2020-07-29 05:53:50 +0800

    finish city-ajax

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,11 +1,6 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li class="item" v-for="(item,key) in cities" :key="key">{{key}}</li>
   </ul>
 </template>
 
@@ -13,6 +8,9 @@
 
 export default {
   name: 'CityAlphabet',
+  props: {
+    cities: Object
+  },
   data () {
     return {
     }
