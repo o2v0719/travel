c22e6375a76896bfd1f721d4d6b31c415206865e
神经蛙 <470408436@qq.com>
2020-07-30 06:22:51 +0800

    finish search logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,17 +1,69 @@
 <template>
-  <div class="search">
-    <input type="text" placeholder="输入城市名或拼音" class="search-input">
+  <div>
+    <div class="search">
+      <input v-model="keyword" type="text" placeholder="输入城市名或拼音" class="search-input">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom" v-show="showFlag">
+          <!-- 当list数组的长度为0的时候才显示 -->
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-
+import Bscroll from 'better-scroll'
 export default {
   name: 'CitySearch',
+  props: {
+    cities: Object
+  },
   data () {
     return {
+      keyword: '',
+      list: [],
+      timer: null,
+      showFlag: false
     }
   },
+  watch: {
+    keyword () {
+      // 为了保证性能，引入节流函数
+      if (this.timer) {
+        clearTimeout(this.timer)
+        this.showFlag = false
+      }
+      // 先输入后删除的时候要清除屏幕上的内容
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            //由于搜索的时候，可能用的是拼音也可能是汉字
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+        if (!this.list.length) {
+          this.showFlag = true
+        }
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -30,4 +82,19 @@ export default {
     text-align center
     border-radius 0.06rem
     color #666
+.search-content
+  z-index 1
+  overflow hidden
+  position absolute
+  // 注意要配合better-scroll 插件
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  background-color #eeeeee
+  .search-item
+    line-height 0.62rem
+    padding-left 0.2rem
+    background-color #fff
+    color #666666
 </style>
