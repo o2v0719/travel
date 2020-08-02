95a765479665e6fa17c992ed6b4d1296da8286e2
神经蛙 <470408436@qq.com>
2020-08-02 22:35:09 +0800

    finish header-animation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// 解决移动端个别浏览器0.3s点击延迟的问题
-import fastClick from 'fastClick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
 //vuex
@@ -15,7 +13,6 @@ import 'styles/border.css'
 
 Vue.config.productionTip = false
 
-fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <span class="iconfont iconarrow-left-bold header-abs-back"></span>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <div class="iconfont iconarrow-left-bold header-fixed-back"></div>
+      景点详情
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  activated () {
+    // scroll事件发生，就会执行handleScroll事件
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll () {
+      // console.log(document.documentElement.scrollTop)
+      const top = document.documentElement.scrollTop || document.body.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = {
+          opacity
+        }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  }
+
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header-abs
+  position absolute
+  left 0.2rem
+  top 0.2rem
+  width 0.8rem
+  height 0.8rem
+  border-radius 0.4rem
+  text-align center
+  line-height 0.8rem
+  background rgba(0, 0, 0, 0.8)
+  .header-abs-back
+    color #fff
+    font-size 0.4rem
+.header-fixed
+  position fixed
+  top 0
+  left 0
+  right 0
+  height $headerHeight
+  line-height $headerHeight
+  text-align center
+  color #fff
+  background $bgColor
+  font-size 0.32rem
+  .header-fixed-back
+    position absolute
+    top 0
+    left 0
+    width 0.64rem
+    text-align center
+    font-size 0.4rem
+    color #fff
+</style>
